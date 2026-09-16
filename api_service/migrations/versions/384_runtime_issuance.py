dbbc8a92b2c243940082719c46eee0be909ea046
Nate Sticco <nsticco@gmail.com>
2026-09-16 23:11:14 +0000

    Rename Alembic revision 384 to fit version_num VARCHAR(32)

--- api_service/migrations/versions/384_runtime_issuance.py
+++ api_service/migrations/versions/384_runtime_issuance.py
@@ -19,7 +19,7 @@ import sqlalchemy as sa
 from alembic import op
 
 
-revision: str = "384_runtime_binding_repository_issuance"
+revision: str = "384_runtime_issuance"
 down_revision: Union[str, None] = "383_terminal_contract_source"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
